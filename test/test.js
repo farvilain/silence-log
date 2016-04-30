1277402783f4edd2fa7dfa24d370f8a4ecb7701c
Francois A.R. Vilain <far.vilain@gmail.com>
2016-04-30 18:40:14 +0200

    Use LogLevel and remove appender from a level if added to another

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 var assert = require('assert');
 var Logger = require('../lib/Logger');
 
+function mock (){
+	var mock = { count : 0 };
+	mock.log = function() { mock.count++ };
+	return mock;
+}
+
 describe("Logger", function(){
 	describe("constructor", function(){
 		it("works", function(){
@@ -69,7 +75,20 @@ describe("Logger", function(){
 			it("don't add on children", function(){
 				assert.deepEqual({"info" : [1,2,3]}, child.appenders);
 			});
-		})
+		});
+
+		describe("that already exists on another level", function(){
+			var log = new Logger("null");
+			log.addAppender("debug",1);
+			log.addAppender("info", 1);
+
+			it("is now on the good level", function(){
+				assert.deepEqual([1], log.appenders['info']);
+			});
+			it("is removed from the old one", function(){
+				assert.deepEqual([], log.appenders["debug"]);
+			});
+		});
 
 		describe("that do not already exist", function(){
 			var parent = new Logger("null");
@@ -84,6 +103,44 @@ describe("Logger", function(){
 			it("add on children", function(){
 				assert.deepEqual({"info" : [1,2,3]}, child.appenders);
 			});
+		});
+	});	
+
+	describe("log", function(){
+
+		describe("call", function(){
+			var debug = mock();
+			var info = mock();
+			var error = mock();
+			var log = new Logger("");
+			log.addAppender("info",info);
+			log.addAppender("error",error);
+			log.addAppender("debug",debug);
+			log.info();
+			log.info();
+
+			it("exact level", function(){
+				assert.strictEqual(2, info.count);
+			});
+
+			it("lower level", function(){
+				assert.strictEqual(2, debug.count);
+			});
+
+			it("never higher level", function(){
+				assert.strictEqual(0, error.count);
+			});
+		});
+
+		it("don't call twice the same logger on different level", function(){
+			var info = mock();
+			var parent = new Logger("parent");
+			parent.addAppender("info", info);
+			var log = new Logger("test", parent);
+			log.addAppender("info",info);
+			log.info();
+
+			assert.strictEqual(1, info.count);
 		});
 	});	
 });
