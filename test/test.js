af2fc39518ac06a861187781e75eeb4565620461
Francois A.R. Vilain <far.vilain@gmail.com>
2016-05-01 20:57:10 +0200

    Maintains full appenders list for each level to increase the log method

--- test/test.js
+++ test/test.js
@@ -65,38 +65,60 @@ describe("Logger", function(){
 			parent.addAppender("info", 3);
 
 			it("don't add on logger", function(){
-				assert.deepEqual({"info" : [1,2,3]}, parent.appenders);
+				assert.deepEqual([1,2,3], parent.appenders.info);
 			});
 			it("don't add on children", function(){
-				assert.deepEqual({"info" : [1,2,3]}, child.appenders);
+				assert.deepEqual([1,2,3], child.appenders.info);
 			});
 		});
 
-		describe("that already exists on another level", function(){
+		describe("that already exists on a lower level", function(){
 			var log = new Logger("null");
 			log.addAppender("debug",1);
 			log.addAppender("info", 1);
 
 			it("is now on the good level", function(){
-				assert.deepEqual([1], log.appenders['info']);
+				assert.deepEqual([1], log.appenders.info);
 			});
-			it("is removed from the old one", function(){
-				assert.deepEqual([], log.appenders["debug"]);
+			it("is not removed from the old one", function(){
+				assert.deepEqual([1], log.appenders.debug);
+			});
+		});
+
+		describe("that already exists on a higher level", function(){
+			var log = new Logger("null");
+			log.addAppender("error",1);
+			log.addAppender("info", 1);
+
+			it("is now on the good level", function(){
+				assert.deepEqual([1], log.appenders.info);
+			});
+			it("is not removed from the old one", function(){
+				assert.deepEqual([1], log.appenders.error);
 			});
 		});
 
 		describe("that do not already exist", function(){
 			var parent = new Logger("null");
-			parent.appenders = {"info" : [1,2]};
 			var child = new Logger("paf", parent);
 
 			parent.addAppender("info", 3);
 
 			it("add on logger", function(){
-				assert.deepEqual({"info" : [1,2,3]}, parent.appenders);
+				assert.deepEqual([3], parent.appenders.info);
 			});
+			it("add on higher logger", function(){
+				assert.deepEqual([3], parent.appenders.warn);
+				assert.deepEqual([3], parent.appenders.error);
+			});
+
+			it("don't add on lower logger", function(){
+				assert.strictEqual(undefined, parent.appenders.debug);
+				assert.strictEqual(undefined, parent.appenders.trace);
+			});
+
 			it("add on children", function(){
-				assert.deepEqual({"info" : [1,2,3]}, child.appenders);
+				assert.deepEqual([3], child.appenders.info);
 			});
 		});
 	});	
